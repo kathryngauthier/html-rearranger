0d789d5545b1993921b5c3e8afed006d5c8e794e
kathryngauthier <Kathryn@Kathryns-MacBook-Pro.local>
2016-03-17 21:12:45 -0400

    fixed file

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+var $body = $('body');
+var $header1 = $('<h2>');
+var $header2 = $('<h2>');
+var $ul = $('ul');
+var $ul1 = $('<ul>');
+var $ul2 = $('<ul>');
+var $li = $('ul li');
+
+$body.append($header2);
+$header2.html('Below ground veggies');
+$body.append($ul2);
+$body.append($header1);
+$header1.html('Above ground veggies');
+$body.append($ul1);
+
+$li.each(function () {
+  if ($(this).hasClass('below')) {
+    $ul2.append($(this));
+  } else {
+    $ul1.append($(this));
+  }
+});
